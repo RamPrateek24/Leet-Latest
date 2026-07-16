983886184cd3547c7649f14490b22f61750847f5
MANTRIPRAGADA RAM PRATEEK <131789883+RamPrateek24@users.noreply.github.com>
2026-07-16 18:43:32 +0530

    Time: 3 ms (18.97%) | Memory: 47.9 MB (20.29%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int p1 = 0, p2 = 0;
+        while(p2 < nums.length){
+            if(nums[p2] != 0){
+                int temp = nums[p1];
+                nums[p1] = nums[p2];
+                nums[p2] = temp;
+                p1++;
+            }
+            p2++;
+        }
+    }
+}
